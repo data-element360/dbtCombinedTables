490f39f6be4a0cefaa84f9fe46915152fb96d868
ghavranek@inlytica.com <ghavranek@inlytica.com>
2021-09-23 15:55:22 +0000

    Contacts / Deals union created.  Need to figure out Concat

--- models/CRM/crmTables/landmarkHubspot.sql
+++ models/CRM/crmTables/landmarkHubspot.sql
@@ -23,11 +23,44 @@
 
 {% if execute %}
 
-{% set results_list = latestContactTable.columns[0].values()[0] %}
+{% set contact_table = latestContactTable.columns[0].values()[0] %}
 
 {% else %}
-{% set results_list = [] %}
+{% set contact_table = [] %}
 {% endif %}
+
+--***************************************************************************
+-- Find latest Deal Table
+
+{%  set getLatestDealTable_query %}
+
+    Select table_id, creation_time  FROM dataraw.hubspotLandmark.__TABLES__
+    WHERE creation_time = 
+        (
+        SELECT MAX(creation_time) AS maxDealCreate
+        FROM dataraw.hubspotLandmark.__TABLES__
+        WHERE CONTAINS_SUBSTR(table_id ,"landmark_deals")
+        )
+
+    AND 
+        CONTAINS_SUBSTR(table_id ,"landmark_deals")
+
+{% endset %}
+
+{% set latestDealTable = run_query(getLatestDealTable_query) %}
+
+{% if execute %}
+
+{% set deal_table = latestDealTable.columns[0].values()[0] %}
+
+{% else %}
+{% set deal_table = [] %}
+{% endif %}
+
+
+
+--***************************************************************************
+-- Use table names to create join of contacts and deals
 
 
 With hubspotContacts As (
@@ -35,17 +68,48 @@ SELECT
     firstname As First_Name,
     lastname As Last_Name,
     email AS Email,
-    createdate AS Created_Date,
+    createdate AS Contact_Created_Date,
     ga_client_id AS GCLID,
     lastmodifieddate AS Last_Modified_Date,
     lead_source As Lead_Source,
-    lifecyclestage AS Stage,
-    CAST(hubspot_owner_id as int) AS Owner_Id,
     contact_type As Contact_Type,
     phone As Phone,
-    hs_created_by_user_id AS Created_By_Id
- FROM `dataraw.hubspotLandmark.{{results_list}}`
+    hs_object_id AS contactId
+FROM `dataraw.hubspotLandmark.{{contact_table}}`
 ),
+hubspotDeals AS (
+
+SELECT 
+    dealname AS Deal_Name,
+    dealstage AS Stage_Original,
+    description AS Deal_Description,
+    amount As Amount,
+    hs_createdate AS Deal_Created_Date,
+    hs_deal_stage_probability AS Deal_Probability,
+    CONCAT(closed_lost_reason, closed_won_reason) AS Deal_Closed_Reason,
+    CONCAT(hs_date_entered_closedlost, hs_date_entered_closedwon) AS Deal_Closed_Date,
+    hs_object_id AS dealId,
+    closed_lost_reason,
+    closed_won_reason
+FROM `dataraw.hubspotLandmark.{{deal_table}}`
+
+),
+
+associations AS (
+SELECT * FROM `dataraw.hubspotLandmark.landmark_dealToContactAssociation`
+),
+
+contactUnionDealId AS (
+    
+SELECT hubspotContacts.*, associations.dealId FROM hubspotContacts 
+LEFT JOIN associations
+ON hubspotContacts.contactId = associations.contactId
+
+)
+
+SELECT * FROM contactUnionDealId 
+LEFT JOIN hubspotDeals ON
+contactUnionDealId.dealId = hubspotDeals.dealId
 
 
 
