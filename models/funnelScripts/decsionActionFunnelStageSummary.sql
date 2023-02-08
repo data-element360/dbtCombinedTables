3f017f7e2824d38a750b71ec2a9d44e39e7577e3
ghavranek@inlytica.com <ghavranek@inlytica.com>
2023-02-08 20:04:23 +0000

    Changed integer tonumeric

--- models/funnelScripts/decsionActionFunnelStageSummary.sql
+++ models/funnelScripts/decsionActionFunnelStageSummary.sql
@@ -1,4 +1,4 @@
-SELECT Client, CRM_Stage_Original AS Stage, branded, COUNT(*) AS decsionAwareCount, SUM(pageviews) AS sumPageViews, SUM(sessionDuration) AS sumSessionDuration
+SELECT Client, CRM_Stage_Original AS Stage, branded, CAST(COUNT(*) AS NUMERIC) AS decsionAwareCount, SUM(pageviews) AS sumPageViews, SUM(sessionDuration) AS sumSessionDuration
 FROM dataproduction.combinedTables.allCombined
 GROUP BY Client, Stage, branded 
 ORDER BY Client
