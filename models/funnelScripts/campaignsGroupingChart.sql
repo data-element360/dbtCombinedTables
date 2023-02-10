9bdb6e3fafde44b12a3456de9132734490782da2
ghavranek@inlytica.com <ghavranek@inlytica.com>
2023-02-10 15:52:06 +0000

    Added Funnel Charts

---
+++ models/funnelScripts/campaignsGroupingChart.sql
@@ -0,0 +1,24 @@
+SELECT googleAnalytics.client AS Client, googleAnalytics.branded AS Branded, campaign AS Campaign, SUM(totalGA) AS numGA, 
+CASE 
+  WHEN SUM(gfCount) IS NULL THEN 0
+  ELSE SUM(gfCount)
+  END  AS numGF,
+ 
+
+FROM
+
+(SELECT client, branded, regexp_extract(CAST(clientId AS STRING), '[^.]*') AS clientId, campaign, COUNT(*) AS totalGA 
+FROM 
+(SELECT DISTINCT client, branded, clientId, sessionCount, pageviews, sessionDuration, campaign, keyword, adContent, source, channelGrouping,
+        region, city, deviceCategory FROM `dataproduction.combinedTables.googleanalyticsKPICombined` )
+GROUP BY client, branded, clientId, campaign) googleAnalytics
+
+
+LEFT JOIN
+
+(SELECT client, regexp_extract(CAST(gaClientId AS STRING), '[^.]*') AS gaClientId, 1 AS gfCount
+FROM `dataproduction.combinedTables.gravityformsCombined`) gravityForms
+
+ON googleAnalytics.client = gravityForms.client AND googleAnalytics.clientId = gravityForms.gaClientId
+
+GROUP BY Client, Branded, Campaign
